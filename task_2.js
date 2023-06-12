362c97d99cd9f311d0f6badef2c9087433a86c6b
Remezov Dmitriy <darknessdizi@gmail.com>
2023-06-13 00:33:34 +0700

    Добавлена запись и чтение файлов в задаче 2

--- task_2.js
+++ task_2.js
@@ -1,5 +1,6 @@
 const colorGreen = (text) => {return '\033[32m' + text + '\033[0m'};
 const colorRed = (text) => {return '\033[31m' + text + '\033[0m'};
+const colorYellow = (text) => {return '\033[33m' + text + '\033[0m'};
 
 const numberForUser = Math.floor(Math.random() * 1000);
 console.log(colorGreen("Исходное число: "), numberForUser);
@@ -9,29 +10,51 @@ const readline = require('readline').createInterface({
     output: process.stdout
 })
 
+const fs = require("fs");
+
 const isInteger = num => /^-?[0-9]+$/.test(num);
 
 function question () {
     count++;
     readline.question(`Попытка № ${count}. Введите число: `, (number) => {
+        dataFile += `Попытка № ${count}. `
         if (number == 'end') {
+            dataFile += 'Конец программы.\n'
             console.log(colorRed('Конец программы.'));
             readline.close();
+            saveFile(dataFile);
         } else if (!isInteger(number)) {
-            console.log(colorRed('Ошибка. Вы ввели не число!!!'));
+            dataFile += `Ошибка. Вы ввели "${number}" - это не число!!!\n`
+            console.log(colorRed(`Ошибка. Вы ввели "${number}" - это не число!!!`));
             question();
         } else if (number == numberForUser) {
-            console.log(colorGreen(`Поздравляем! Вы угадали!!! Общее колличество попыток ${count}.`));
+            dataFile += `Поздравляем! Вы угадали число!!! Общее колличество попыток ${count}.\n`
+            console.log(colorGreen(`Поздравляем! Вы угадали число!!! Общее колличество попыток ${count}.`));
             readline.close();
+            saveFile(dataFile);
         } else if (number < numberForUser) {
-            console.log('Ваше число меньше.');
+            dataFile += `Ваше число ${number} меньше загаданного.\n`
+            console.log(`Ваше число ${number} меньше загаданного.`);
             question();
         } else if (number > numberForUser) {
-            console.log('Ваше число больше.');
+            dataFile += `Ваше число ${number} больше загаданного.\n`
+            console.log(`Ваше число ${number} больше загаданного.`);
             question();
         };
     });
 };
 
+function saveFile (data) {
+    fs.writeFile("logs.txt", data, function(error) {
+        if (error) throw error; // если возникла ошибка
+        console.log(colorYellow("Асинхронная запись файла завершена. Содержимое файла:"));
+        let data = fs.readFileSync("logs.txt", "utf8");
+        console.log(data);  // выводим считанные данные
+    });
+};
+
 let count = 0;
+let dataFile = '';
 question();
+
+ 
