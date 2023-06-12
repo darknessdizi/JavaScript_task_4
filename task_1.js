2fa558fdb5828ad3fcab9b3d3d0cc16e5df406ff
Remezov Dmitriy <darknessdizi@gmail.com>
2023-06-12 23:39:16 +0700

    решена задача 1 и 2

--- task_1.js
+++ task_1.js
@@ -8,17 +8,23 @@ function getPasswordChecker(password) {
 function getPasswordChecker_2(password) {return (text) => {return password === text}};
 
 
+const colorGreen = (text) => {return '\033[32m' + text + '\033[0m'};
+const colorMagenta = (text) => {return '\033[35m' + text + '\033[0m'};
+const colorCyan = (text) => {return '\033[36m' + text + '\033[0m'};
+
+
 const comparePassword = getPasswordChecker('123qwe');
 
-console.log('Функция №1', comparePassword(1234), '1234');
-console.log('Функция №1', comparePassword('tyufg'), 'tyufg');
-console.log('Функция №1', comparePassword('123qwe'), '123qwe');
+console.log(colorMagenta('Функция №1'), comparePassword(1234), colorCyan('1234'));
+console.log(colorMagenta('Функция №1'), comparePassword('tyufg'), colorCyan('tyufg'));
+console.log(colorGreen('Функция №1'), colorGreen(comparePassword('123qwe')), colorGreen('123qwe'));
 
 const comparePassword_2 = getPasswordChecker_2('1qaz2wsx');
 
 console.log('');
-console.log('Функция №2', comparePassword_2(9876), '9876');
-console.log('Функция №2', comparePassword_2('edgrh'), 'edgrh');
-console.log('Функция №2', comparePassword_2(null), 'null');
-console.log('Функция №2', comparePassword_2('1qaz2wsx'), '1qaz2wsx');
-console.log('Функция №2', comparePassword_2(undefined), 'undefined');
+console.log(colorMagenta('Функция №2'), comparePassword_2(9876), colorCyan('9876'));
+console.log(colorMagenta('Функция №2'), comparePassword_2('edgrh'), colorCyan('edgrh'));
+console.log(colorMagenta('Функция №2'), comparePassword_2(null), colorCyan('null'));
+console.log(colorGreen('Функция №2'), colorGreen(comparePassword_2('1qaz2wsx')), colorGreen('1qaz2wsx'));
+console.log(colorMagenta('Функция №2'), comparePassword_2(undefined), colorCyan('undefined'));
+
--- task_2.js
+++ task_2.js
@@ -1,16 +1,37 @@
-const numberForUser = Math.floor(Math.random() * 1000)
-console.log("Исходное число: ", numberForUser)
-while (true) {
-    let numberFromUser = +prompt("Введите загаданное число: ")
-    console.log("Число пользователя: ", numberFromUser)
-    if (isNaN(numberFromUser)) {
-        alert("Вы ввели не число")
-    } else if (numberFromUser === numberForUser) {
-        alert("Вы угадали !!!")
-        break
-    } else if (numberFromUser < numberForUser) {
-        alert("Ваше число меньше.")
-    } else if (numberFromUser > numberForUser) {
-        alert("Ваше число больше.")
-    }
-}
+const colorGreen = (text) => {return '\033[32m' + text + '\033[0m'};
+const colorRed = (text) => {return '\033[31m' + text + '\033[0m'};
+
+const numberForUser = Math.floor(Math.random() * 1000);
+console.log(colorGreen("Исходное число: "), numberForUser);
+
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+const isInteger = num => /^-?[0-9]+$/.test(num);
+
+function question () {
+    count++;
+    readline.question(`Попытка № ${count}. Введите число: `, (number) => {
+        if (number == 'end') {
+            console.log(colorRed('Конец программы.'));
+            readline.close();
+        } else if (!isInteger(number)) {
+            console.log(colorRed('Ошибка. Вы ввели не число!!!'));
+            question();
+        } else if (number == numberForUser) {
+            console.log(colorGreen(`Поздравляем! Вы угадали!!! Общее колличество попыток ${count}.`));
+            readline.close();
+        } else if (number < numberForUser) {
+            console.log('Ваше число меньше.');
+            question();
+        } else if (number > numberForUser) {
+            console.log('Ваше число больше.');
+            question();
+        };
+    });
+};
+
+let count = 0;
+question();
